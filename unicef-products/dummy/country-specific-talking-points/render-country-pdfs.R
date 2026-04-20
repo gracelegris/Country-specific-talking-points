2341123f47ae794545b72740253a7e77735b9a77
UNICEF <UNICEF@MacBook-Air-4.local>
2026-04-20 15:05:21 -0700

    draft 1 of country talking points

--- unicef-products/dummy/country-specific-talking-points/render-country-pdfs.R
+++ unicef-products/dummy/country-specific-talking-points/render-country-pdfs.R
@@ -4,16 +4,19 @@ library(tinytex)
 library(readxl)
 library(openxlsx)
 library(patchwork)
+library(tidytext)
 
 comp_yr <- 2019
 
 ## setup ----
 source("unicef-products/main_vars.R")  # key vars
 source(str_glue("unicef-products/{type}/utils/R/label_vals.R"))  # labeling function
+source(str_glue("unicef-products/{type}/country-specific-talking-points/utils/user_profiles.R"))
 
 # wuenic data
 wuenic_dta <- read_rds(str_glue("unicef-products/{type}/01_wuenic_dataset-prep/clean_wuenic_MASTER_{rev_yr}rev.rds")) %>%
-  #filter(lvl_2 %in% c("region_unicef_ops", "region_au", "region_au_africa") %>% 
+  #filter(lvl_2 %in% c("region_unicef_ops", "region_au", "region_au_africa")) %>% 
+  filter(lvl_2 %in% c("region_unicef_ops")) %>% 
   filter(year >= 2000) %>%
   mutate(country = case_when(iso3c == "bol" ~ "Bolivia",
                              # iso3c == "caf" ~ "CAR",
@@ -36,6 +39,9 @@ wuenic_dta <- read_rds(str_glue("unicef-products/{type}/01_wuenic_dataset-prep/c
 
 # hpv data
 hpv_dta <- read_excel(str_glue('unicef-products/{type}/region-specific-talking-points/utils/hpv_estimates_wuenic{hpv_rev_yr}rev.xlsx')) %>%
+  #filter(lvl_2 %in% c("region_unicef_ops", "region_au", "region_au_africa")) %>% 
+  filter(lvl_2 %in% c("region_unicef_ops")) %>% 
+  filter(!is.na(coverage)) %>% 
   # programme coverage
   filter(vaccine_code %in% c("PRHPV1_F", "PRHPVC_F")) %>%
   #filter(lvl_2 %in% c("region_unicef_ops", "region_au", "region_au_africa")) %>%
@@ -57,8 +63,11 @@ base_map_df <- readRDS(str_glue('unicef-products/{type}/utils/unicef-base-map.rd
   sf::st_as_sf() %>% 
   mutate_at(vars(iso3c), str_to_lower)
 
-# unique regions ----
-countries <- unique(wuenic_dta$Country) 
+# unique countries ----
+countries <- unique(wuenic_dta$country)
+
+# filter to num 1 to 195
+countries <- countries[1:195]
 
 ## render pdfs ----
 # loop through regions and generate reports
@@ -68,7 +77,7 @@ for (country in countries) {
   
   rmarkdown::render(str_glue("unicef-products/{type}/country-specific-talking-points/country_report_template.Rmd"),
                     output_file = output_file,
-                    params = list(region = reg),
+                    params = list(country = country),
                     envir = new.env())  # ensure a clean environment
   
   message("Report generated: ", output_file)
